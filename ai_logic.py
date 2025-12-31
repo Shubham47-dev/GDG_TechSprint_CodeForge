01e7832f812b2bd94f7e28be08e6f2224a227793
Shubham47-dev <shubhamsahay138@gmail.com>
2025-12-31 23:57:10 +0530

    API bug fixed

--- ai_logic.py
+++ ai_logic.py
@@ -2,10 +2,15 @@ import streamlit as st
 import google.generativeai as genai
 import os
 import json
-from dotenv import load_dotenv
 
-load_dotenv()
-API_KEY = os.getenv("GEMINI_API_KEY")
+
+if "GEMINI_API_KEY" in st.secrets:
+    api_key = st.secrets["GEMINI_API_KEY"]
+
+else:
+    from dotenv import load_dotenv
+    load_dotenv()
+    API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key = API_KEY)
 model = genai.GenerativeModel(
