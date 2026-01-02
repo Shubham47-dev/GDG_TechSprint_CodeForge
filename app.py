9d78121a3ed3837772a19cdaaca8e2a6ddcfa840
Shubham47-dev <shubhamsahay138@gmail.com>
2026-01-02 15:33:21 +0530

    Add session state after project completion

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ st.set_page_config(
     layout="wide"
 )
 
+if 'analysis_data' not in st.session_state:
+    st.session_state.analysis_data = None
+
 with st.container():
     col_a, col_b = st.columns([3, 1])
     with col_a:
@@ -51,8 +54,22 @@ if st.button("Analyze Resume Now", type="primary", use_container_width=True):
             pg = pdf_utils.resume_len(uploaded_file)
         
         with st.spinner("Consulting Gemini AI..."):
-            analysis = ai_logic.analyze_resume(resume_text, job_desc)
+            st.session_state.analysis_data = ai_logic.analyze_resume(resume_text, job_desc)
         st.success("Analysis Complete!")
+
+    elif uploaded_file and not job_desc:
+        st.error("⚠️ Please paste the Job Description to start!")
+
+    elif not uploaded_file and job_desc:
+        st.error("⚠️ Please upload your Resume to start!")
+
+    elif not uploaded_file and not job_desc:
+        st.error("⚠️ Please paste the Job Description and Upload your Resume to start!")
+    
+if st.session_state.analysis_data:
+        
+        analysis = st.session_state.analysis_data
+        resume_text = pdf_utils.get_text(uploaded_file)
 
         with st.container(border = True):
 
@@ -84,7 +101,7 @@ if st.button("Analyze Resume Now", type="primary", use_container_width=True):
                 elif analysis['ATS_Readability'] == 'Low':
                     st.markdown(f"###### ATS Readability: :red[{analysis['ATS_Readability']}]")
             st.divider()
-        
+
             col1, col2 = st.columns([1, 1.6])
             
             with col1:
@@ -105,8 +122,46 @@ if st.button("Analyze Resume Now", type="primary", use_container_width=True):
                 
             with col2:
                 st.subheader("Analysis Report 📊")
+                st.write("")
+                if(len(analysis['missing_keywords'])): 
+                    st.markdown("**🧩 Missing Critical Skills**")
+                    chip_style = """
+                        <style>
+                        .chip-container {
+                        display: flex;
+                        flex-wrap: wrap;
+                        gap: 8px;
+                        margin-top: 5px;
+                        margin-bottom: 20px;
+                        }
+                        .chip {
+                            background-color: rgba(255, 75, 75, 0.15); /* Light red background */
+                            color: #ff4b4b;                             /* Dark red text */
+                            border: 1px solid #ff4b4b;
+                            padding: 6px 14px;
+                            border-radius: 20px;
+                            font-size: 14px;
+                            font-weight: 600;
+                            font-family: sans-serif;
+                            }
+                        </style>
+                        """
+        
+                    chips_html = '<div class="chip-container">'
+                    for keyword in analysis['missing_keywords']:
+                        chips_html += f'<div class="chip">{keyword}</div>'
+                    chips_html += '</div>'
+        
+                        # Render it
+                    st.markdown(chip_style + chips_html, unsafe_allow_html=True)
+
+                else:
+                    st.success("✅ No critical keywords missing!")
+                
+                st.divider()
+
                 st.markdown("**🚨 Critical Gaps**")
-                st.info(analysis['critical_gaps']) 
+                st.info(analysis['critical_gaps'])
                 st.divider()
 
                 st.markdown("**🗣️ Soft Skills**")
@@ -128,19 +183,43 @@ if st.button("Analyze Resume Now", type="primary", use_container_width=True):
             col1, col2 = st.columns(2)
         
             with col1:
-                with st.expander("View Extracted Resume Text"):
-                    st.caption(f"Page Count: {pg}")
-                    st.text_area("Raw Text", resume_text, height=200, disabled=True)
+                with st.expander("View Raw ATS Text (What ATS would sees)"):
+                    st.info("This is how an ATS reads your resume. If your text looks scrambled here, the algorithms can't read your skills!")
+                    st.code(resume_text, language='text')
         
             with col2:
                 with st.expander("View Raw AI JSON"):
                     st.json(analysis)
+        job_title_snippet = job_desc[:50] + "..." if len(job_desc) > 50 else job_desc
+                
+        report_content = f"""
+        JOBFIT AI REPORT
+        ----------------
+        Match Score: {analysis['match_percentage']}%
+        Job Role: {job_title_snippet}
 
-    elif uploaded_file and not job_desc:
-        st.error("⚠️ Please paste the Job Description to start!")
+        MISSING SKILLS:
+        {', '.join(analysis['missing_keywords'])}
 
-    elif not uploaded_file and job_desc:
-        st.error("⚠️ Please upload your Resume to start!")
+        ADVICE:
+        {analysis['advice']}
 
-    elif not uploaded_file and not job_desc:
-        st.error("⚠️ Please paste the Job Description and Upload your Resume to start!")
+        Generated by CodeForge
+        """
+        st.markdown("""
+            <style>
+            div[data-testid="stDownloadButton"] > button {
+                color: #e8f7f7ff;
+                background: #8b5cf6;
+                font-weight: bold;
+            }
+            </style>
+        """, unsafe_allow_html=True)
+
+        st.download_button(
+            label="Download Full Report ",
+            data=report_content,
+            file_name="JobFit_Analysis.txt",
+            mime="text/plain",
+            use_container_width=True
+        )
